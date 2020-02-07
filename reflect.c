6486baedaf518956cc093308ec9e0577f291bd7a
Jason Nyberg <jasonnyberg@gmail.com>
2020-02-07 12:20:36 -0500

    cosmetic

--- reflect.c
+++ reflect.c
@@ -69,10 +69,6 @@ static void init(void)
     try_depth=1;
     cif_preview_module(cif_module);
 
-    //LTI *lti=LTI_resolve(cif_module,"/home/jnyberg/j2/listree.c",false);
-    //LTV_erase(cif_module,lti);
-    //LTV_erase(cif_module,LTI_find(cif_module,"*/rbtree.c",false,LT_NONE));
-
     print_ltv(ERRFILE,CODE_RED,cif_module,CODE_RESET "\n",0);
     cif_curate_module(cif_module,true);
 }
